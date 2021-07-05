2f65e94904f0d9f1768aa1429342bb67c5ce7af8
PelaNisha <nishapela777@gmail.com>
2021-07-05 22:12:58 +0545

    commited

--- letter/views.py
+++ letter/views.py
@@ -3,18 +3,19 @@ from .models import post
 from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 class HomeView(ListView):
-    models = post
+    model = post
     template_name = 'base.html'
 
     def get_queryset(self):
       return post
 
 class NewsDetailView(DetailView):
-    models = post
+    model = post
     template_name = 'new_details.html'
+    
 
 class AddNewView(CreateView):
-    models = post
+    model = post
     template_name = 'addpost.html'
     fields = '__all__'
     
